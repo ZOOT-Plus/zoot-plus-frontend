e680732acee4d9b43576e090f7f769c4a959fa68
quyansiyuanwang <2301179475@qq.com>
2026-03-25 00:14:47 +0800

    fix: 调整蛇形布局为链式布局及主题适配

--- src/store/pref.ts
+++ src/store/pref.ts
@@ -1,4 +1,4 @@
 import { atomWithStorage } from 'jotai/utils'
 
 export const neoLayoutAtom = atomWithStorage('neo-layout', true)
-export const snakeModeAtom = atomWithStorage('snake-mode', false)
+export const gridModeAtom = atomWithStorage('grid-mode', false)
